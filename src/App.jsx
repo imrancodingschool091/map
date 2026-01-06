43061f20122930601a652c50a8423231b6960b8a
imrancodingschool <imrancodingshool@gmail.com>
2026-01-06 23:33:30 +0530

    new changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
-import {Routes,Route} from "react-router-dom"
+import {Routes,Route,Link} from "react-router-dom"
 import Home from './pages/Home'
 import DriverLocation from './pages/DriverLocation'
 
 const App = () => {
   return (
     <div>
+      <Link to={"/location"}>Location</Link>
       <Routes>
         <Route  path='/' element={<Home/>}/>
          <Route  path='/location' element={<DriverLocation/>}/>
